bf610792bd6aeecee410c56b84f08a8d5da0ae76
Diba Mirza <dimirza@cs.ucsb.edu>
2018-11-01 10:53:25 -0700

    lec9

--- lec09/accumulator.py
+++ lec09/accumulator.py
@@ -1,44 +1,107 @@
-# accumulator.py : Patterns of programming using loops, nested loops
+# accumulator.py :
+# Solving problems that involve collections (strings, lists, tuples, namedtuples)
+# Patterns of programming using loops, nested loops
 # Nov 1, 2018
 
 def countElements(lst):
   '''returns the number of elements in lst'''
-  return "stub"
+  count = 0
+  for item in lst:
+    count = count+1
+  return count
 
 
 def countOddNumbers(lst):
   '''returns the number of odd numbers in lst'''
-  return "stub"
+  count = 0
+  for item in lst:
+    if(item % 2 == 1):
+      count = count+1
+  return count
 
 
 def countWords(sentence):
   '''returns the number of words in the sentence'''
-  return "stub"
+  count = 0
+  for item in sentence:
+    if(item == ' '):
+      count = count+1
+  return count+1
+
+def countWords_2(sentence):
+  '''returns the number of words in the sentence'''
+
+  lst = sentence.split()
+  count = 0
+  for item in lst:
+      count = count+1
+  return count
 
 
-def countWords(sentence, len):
+
+def countWords_3(sentence, len):
   '''returns the number of words in the sentence with length greater than len'''
   return "stub"
 
 
 def createOddList(lst):
   '''returns a new list that contains all the odd numbers in lst'''
-  return "stub"
+  newlist = []
+  for item in lst:
+    if(item%2 == 1):
+      # item is odd
+      #append item to newlist
+      newlist.append(item)  # add item to newlist
+  #    newlist  = newlist+ [item] (also works)
+  
+  return newlist
 
 
 def drawRectangle(width, height):
   '''print a rectangle with given width and height using the character *
     (instead of turtle)'''
+  rect =''
+  for i in range(height):
+    #Write code that creates a string of width '*'s  and one '\n'
+    rect = rect + '*'*width + '\n'
+
+  print(rect)
+  return
+
+def drawRectangle_2(width, height):
+  '''print a rectangle with given width and height using the character *
+    (instead of turtle)'''
+  rect =''
+  for i in range(height):
+    #Write code that creates a string of width '*'s  and one '\n'
+    line =''
+    for j in range(width):
+      line= line+ '*'
+    print(line)
   return
 
 def drawTriangle(height):
   '''print a right triagle with given height using stars(*).
      Assume the size of the base and height are equal'''
-  return
+  for i in range(1, height+1):
+    # print ith row, print i '*'s
+    print('*'*i)
+    
 
 
- def countVowels(strList):
+def countVowels(strList):
   """ Count vowels in list of strings """
+  ''' [ 'This' , 'is', 'an', 'apple'] ->return 5'''
+  vowels = 'AEIOUaeiou'
+  count = 0
+  for item in strList:
+    for c in item:
+      if c in vowels:
+        count = count +1
+        
+  return count        
+    
+  
  
     
 
