2182de5c70375ab3425ed43ebd49502f93602d1b
Diba Mirza <dimirza@cs.ucsb.edu>
2018-11-01 09:28:21 -0700

    lec9

---
+++ lec09/accumulator.py
@@ -0,0 +1,48 @@
+# accumulator.py : Patterns of programming using loops, nested loops
+# Nov 1, 2018
+
+def countElements(lst):
+  '''returns the number of elements in lst'''
+  return "stub"
+
+
+def countOddNumbers(lst):
+  '''returns the number of odd numbers in lst'''
+  return "stub"
+
+
+def countWords(sentence):
+  '''returns the number of words in the sentence'''
+  return "stub"
+
+
+def countWords(sentence, len):
+  '''returns the number of words in the sentence with length greater than len'''
+  return "stub"
+
+
+def createOddList(lst):
+  '''returns a new list that contains all the odd numbers in lst'''
+  return "stub"
+
+
+def drawRectangle(width, height):
+  '''print a rectangle with given width and height using the character *
+    (instead of turtle)'''
+  return
+
+def drawTriangle(height):
+  '''print a right triagle with given height using stars(*).
+     Assume the size of the base and height are equal'''
+  return
+
+
+ def countVowels(strList):
+  """ Count vowels in list of strings """
+ 
+    
+
+
+
+
+
